20016c4d5dba81a4247dd69bba974b46934863cd
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2011-10-26 17:27:16 +0900

    test with i586-mngw32-cc (Win32 ABI compiler)
    - i586-mngw32-cc compiles the code, using the GNUWin32 headers
    - the libpng and zlib DLLs are temporary linked into the program folder
    - wine executes the programs
    - the output is converted with dos2unix

--- test/01-build.sh
+++ test/01-build.sh
@@ -6,15 +6,16 @@
 # known image, lena. The expected output is in the data folder.
 _test_run() {
     TEMPFILE=$(tempfile)
-    example/mmms data/lena_g.png >> $TEMPFILE
-    example/mmms data/lena_ga.png >> $TEMPFILE
-    example/mmms data/lena_rgb.png >> $TEMPFILE
-    example/mmms data/lena_rgba.png >> $TEMPFILE
+    ./example/mmms data/lena_g.png >> $TEMPFILE
+    ./example/mmms data/lena_ga.png >> $TEMPFILE
+    ./example/mmms data/lena_rgb.png >> $TEMPFILE
+    ./example/mmms data/lena_rgba.png >> $TEMPFILE
+    dos2unix $TEMPFILE
     test "9edef44b78fca2d0f129d3c5b788292b  $TEMPFILE" \
 	= "$(md5sum $TEMPFILE)"
     rm -f $TEMPFILE
     # test all the read-write code variants
-    example/readpng data/lena_g.png
+    ./example/readpng data/lena_g.png
 }
 
 # Test the need for a dynamic libpng library
@@ -36,12 +37,26 @@ _log make clean
 _log make
 
 echo "* compiler support"
-for CC in cc c++ c89 c99 gcc g++ tcc nwcc clang icc pathcc suncc; do
+for CC in cc c++ c89 c99 gcc g++ tcc nwcc clang icc pathcc suncc \
+    i586-mingw32msvc-cc; do
     which $CC || continue
     echo "* $CC compiler"
     _log make distclean
-    _log make CC=$CC
+    case $CC in
+	"i586-mingw32msvc-cc")
+	    test -d ./win32/lib || break
+	    # default mingw behaviour has extra precision
+	    _log make CC=$CC CFLAGS="-O2 -ffloat-store"\
+		CPPFLAGS="-DNDEBUG -I. -I./win32/include" \
+		LDFLAGS="-L./win32/lib"
+	    ln -f -s ../win32/bin/libpng3.dll ../win32/bin/zlib1.dll ./example/
+	    ;;
+	*)
+	    _log make CC=$CC
+	    ;;
+    esac
     _log _test_run
+    rm -f ./example/*.dll
 done
 
 echo "* build with embedded libraries"
